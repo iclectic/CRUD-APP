9a01f42c620048c5c2be6b5dfe98a152c44a6b3f
iclectic <tansonestost@yahoo.com>
2024-04-05 21:20:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello from Node API Server, How ya doing?"); 
+});
+
+
+mongoose.connect("Insert MongoDB Connection String Here")
+.then(() => {
+    console.log("Connected to database!");
+})
+.catch(() => {
+    console.log("Connection failed!");
+});
